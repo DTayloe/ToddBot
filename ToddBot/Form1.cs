65b7b1a694741535a956b09aa78075afdeee7f7a
DTayloe <diomoidapple@gmail.com>
2022-03-28 22:21:27 -0400

    Add project files.

---
+++ ToddBot/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToddBot
+{
+     public partial class Form1 : Form
+     {
+          string[] sounds = {
+               @"C:\Users\diomo\Documents\Audacity\Hello.wav",
+               @"C:\Users\diomo\Documents\Audacity\Meeroh.wav",
+               @"C:\Users\diomo\Documents\Audacity\Woah.wav"
+          };
+
+          public Form1()
+          {
+               InitializeComponent();
+          }
+
+          private void button1_Click(object sender, EventArgs e)
+          {
+               PlayWav(sounds[0]);
+          }
+
+          public void PlayWav(string filePath)
+          {
+               System.Media.SoundPlayer player = new System.Media.SoundPlayer(filePath);
+               player.Play();
+          }
+     }
+}
